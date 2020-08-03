49cd78fd35af121e602b1e9b61026514cd658902
Julian Villegas <julian.villegasplus@gmail.com>
2020-08-03 12:20:49 -0500

    [ADD]first commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const element = <h1>Hello, Platzi Badges!</h1>;
+// Usando el API del Dom
+/* const element = document.createElement('h1');
+element.innerText = 'Hello, Platzi Badges!';
 
 const container = document.getElementById('app');
 
+container.appendChild(element); */
+
+// Usando React
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Esto es JSX, funciona cuando se importa React
+// const jsx = <h1>Hello, Platzi Badges from React</h1>;
+
+/* Alternativa de JSX, usando React.createElement, el segundo parámetro son los atributos o props, el tercero
+es llamado children, parecido al innerText*/
+// const element = React.createElement("h1", {}, "Hola, somos los children");
+
+/* const element = React.createElement(
+  "a",
+  { href: "https://platzi.com" },
+  "Ir a Platzi"
+); */
+
+const name = "Julian";
+// const element = React.createElement("h1", {}, `Hola, soy ${name}`);
+
+//Con JSX, {} permite incluir expresiones de JS (algo que se interpreta y evalúa), operaciones aritméticas y funciones
+// const jsx = <h1>Hola, Soy {name}</h1>;
+const jsx = (
+  <div>
+    <h1>Hola, soy {name}</h1>
+    <p>Soy ingeniero de Sistemas</p>
+  </div>
+);
+const container = document.getElementById("app");
+
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
