6ab5df01458b29bc1548f7edb16c5fe47f490c27
Julian Villegas <julian.villegasplus@gmail.com>
2020-08-05 23:14:56 -0500

    [UPD]BadgeNew: Adding props to BadgeForm with state and setState, also adding props to Badge component

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -6,6 +6,29 @@ import headerLogo from "../images/badge-header.svg";
 import "./styles/BadgeNew.css";
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: "",
+    },
+  };
+
+  handleChange = (e) => {
+    // Haciendo una copia de cada valor en form para ir creando las keys, hay 2 formas de hacerlo
+    // Con la copia declarativa o con el spread
+    /*     const nextForm = this.state.form;
+    nextForm[e.target.name] = e.target.value; */
+
+    this.setState({
+      form: {
+        // Con esto recupera todos los valores que hay en el formulario, antes de sobrescribir
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
   render() {
     return (
       <div>
@@ -17,15 +40,19 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName="Julian"
-                lastName="Villegas"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
                 avatarUrl="https://avatars1.githubusercontent.com/u/55106332?s=460&u=0b22d3549bc5949923266768afe212c3e9316b1d&v=4"
-                jobTitle="Software Engineer"
-                twitter="julianchok25"
+                jobTitle={this.state.form.jobTitle}
+                twitter={this.state.form.twitter}
+                email={this.state.form.email}
               />
             </div>
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>
