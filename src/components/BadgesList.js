d3d9a814fe28e8ff347323841f05a5e379de29f5
Julian Villegas <julian.villegasplus@gmail.com>
2020-08-07 21:37:16 -0500

    [ADD]BadgesList: Adding design to the badges list and reference the values

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "./styles/BadgesList.css";
+import twitterLogo from "../images/Twitter_Logo_Blue.png";
 
 // Es un componente que tiene una responsabilidad única y que le pertenece
 
@@ -9,10 +11,27 @@ export default class BadgesList extends Component {
         {/* Map recibe una función como argumneto, recorre cada uno de los elementos y tiene que regresar otro valor */}
         {this.props.badges.map((badge) => {
           return (
-            <li key={badge.id}>
-              <p>
-                {badge.firstName} {badge.lastName}
-              </p>
+            <li key={badge.id} className="BadgesList__list">
+              <img
+                className="BadgesList__avatar"
+                src={badge.avatarUrl}
+                alt="Avatar"
+              />
+              <div>
+                <p className="BadgesList__name">
+                  {badge.firstName} {badge.lastName}
+                </p>
+                <img
+                  className="BadgesList__twitter-logo"
+                  src={twitterLogo}
+                  alt="Twitter Logo"
+                />
+                <span className="BadgesList__name-twitter">
+                  {badge.twitter}
+                </span>
+                <br />
+                {badge.jobTitle}
+              </div>
             </li>
           );
         })}
