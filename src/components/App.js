7e4381d5b77864f0d7683bd03af8b92f6ccd6fb7
Julian Villegas <julian.villegasplus@gmail.com>
2020-08-09 20:16:27 -0500

    [ADD]BadgeDetailsContainer: Separate the logic with the UI component for good practices in react

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Badges from "../pages/Badges";
 import BadgeNew from "../pages/BadgeNew";
 import BadgeEdit from "../pages/BadgeEdit";
-import BadgeDetails from "../pages/BadgeDetails";
+import BadgeDetails from "../pages/BadgeDetailsContainer";
 
 export default function App() {
   // Al usar una función, no es necesario el método render() como cuando se define después de la clase,
---
+++ src/pages/BadgeDetailsContainer.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import PageLoading from "../components/PageLoading";
+import PageError from "../components/PageError";
+import BadgeDetails from "./BadgeDetails";
+
+import api from "../api";
+
+export default class BadgeDetailsContainer extends Component {
+  state = {
+    loading: true,
+    error: null,
+    data: undefined,
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    this.setState({ loading: true, error: null });
+    try {
+      const data = await api.badges.read(this.props.match.params.badgeID);
+      this.setState({ loading: false, data: data });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
+  render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
+    if (this.state.error) {
+      return <PageError error={this.state.error} />;
+    }
+
+    return <BadgeDetails badge={this.state.data} />;
+  }
+}
